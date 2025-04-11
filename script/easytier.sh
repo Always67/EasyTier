d0c43d01edf93538e994da0c4d1149423da65d05
Always67 <42529007+Always67@users.noreply.github.com>
2025-04-11 11:28:32 +0800

    Update easytier.sh

--- script/easytier.sh
+++ script/easytier.sh
@@ -2,12 +2,8 @@
 
 # 定义镜像源列表
 MIRROR_URLS=(
-     "https://gcore.jsdelivr.net/gh/Always67/EasyTier/script"
-     "https://fastly.jsdelivr.net/gh/Always67/EasyTier/script"
-     "https://testingcf.jsdelivr.net/gh/Always67/EasyTier/script"
-     "https://quantil.jsdelivr.net/gh/Always67/EasyTier/script"
-    "https://gh.ddlc.top/https://raw.githubusercontent.com/Always67/EasyTier/main/script"
-    "https://raw.githubusercontent.com/CGG888/EasyTier/main/script"
+
+    "https://ghfast.top/https://raw.githubusercontent.com/Always67/EasyTier/main/script"
 )
 
 # 测试镜像源速度并排序
